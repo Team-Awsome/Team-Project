fb7a8264c6838d6031e191adae07a9cd6649722c
ContinuumCollapse <interloperprevention@gmail.com>
2025-12-15 13:28:43 -0800

    Skip button

--- My project/Assets/Scripts/DespawnC3.cs
+++ My project/Assets/Scripts/DespawnC3.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class dialogueControl3 : MonoBehaviour
 {
+    public bool mouse = true;   
     public float time = 1f;
     GameObject enemies;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Skip()
     {
-
+        
         Destroy(gameObject);
         enemies.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
     }
     void Start()
     {
+        mouse = true;
         enemies = GameObject.FindWithTag("ENEMIES");
         StartCoroutine(Despawn());
 
@@ -23,13 +27,16 @@ public class dialogueControl3 : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
+        if (mouse)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
         
     }
     IEnumerator Despawn()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+       
 
        
         
@@ -38,11 +45,9 @@ public class dialogueControl3 : MonoBehaviour
 
         yield return new WaitForSeconds(time);
 
+        Destroy(gameObject);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        Destroy(gameObject);
-       
         enemies.SetActive(true);
     }
     
